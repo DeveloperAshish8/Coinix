ac0578dac5e45e636a925b5691f317f1930a6b57
Ashish Kumar <90233808+DeveloperAshish8@users.noreply.github.com>
2024-01-04 16:10:58 +0530

    Add files via upload

---
+++ src/components/Exchange.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import Header from './Header'
+import axios from 'axios'
+import { Baseurl } from './baseUrl'
+import Loader from './Loader'
+import Model from './Model'
+
+const Exchange = () => {
+  const [Loading, setLoading] = useState(true);
+  const [currency, setCurrency] = useState("inr");
+  const currencySymbol = currency === "inr" ? "₹" : "$";
+  const[exchanges, setExchanges]=useState([])
+  useEffect(() => {
+    const getData = async () => {
+      try{
+      const {data} = await axios.get(`${Baseurl}/exchanges`)
+      console.log(data);
+      setLoading(false);
+      setExchanges(data);
+      } catch(error){
+        console.log("API LIMIT EXCEED");
+        setLoading(false);
+      }
+    };
+    getData();
+  },[])
+  return (
+
+    <>
+    {
+    Loading ? <Loader/> : <>
+    <Header/>
+    <Model/>
+      <div>
+        {
+        exchanges.map((item,i)=> {
+          return(
+        <div key={i} className="ex-cards"> 
+           <div className="image">
+            <img src={item.image} alt="coinLogo" height={"40px"}/>
+           </div>
+           <div className="name">{item.name}</div>
+           <div className="price">{currencySymbol}{" "}{item.trade_volume_24h_btc.toFixed(0)}</div>
+           <div className="rank">{item.trust_score_rank}</div>
+        </div>
+        )
+      })
+      }
+      </div>
+    </>
+    }
+    </>
+  )
+}
+
+export default Exchange
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='navbar' >
+    <div className="logo">
+        <Link to='/' style={{textDecoration: "none", color:"white"}}><h1>Coinix </h1> </Link>
+    </div>
+  <ul>
+    <li> <Link to='/coins'>Coins</Link></li>
+  </ul>
+</div>
+  )
+}
+
+export default Header
